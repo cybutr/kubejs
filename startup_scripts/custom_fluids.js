d3b6050f1afa9288b534fa2af33cd27d30197f3a
cybutr <simekadam007@gmail.com>
2025-02-09 14:22:11 +0100

    Create custom_fluids.js, sa_items.js

---
+++ startup_scripts/custom_fluids.js
@@ -0,0 +1,7 @@
+StartupEvents.registry('fluid', event => {
+    event.create('liquid_garnish')
+    .thinTexture(0xbed3c2)
+    .bucketColor(0xbed3c2)
+    .displayName('Garnish')
+    .noBucket()
+})
